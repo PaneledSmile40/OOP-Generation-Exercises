0374874ee048a3feca345d9dc5e3d7dac7622c6e
PaneledSmile40 <matthew.iis.davinci@gmail.com>
2025-07-16 12:52:38 +0200

    ex02 compleated

--- src/com/asyncLessons/Ex02.java
+++ src/com/asyncLessons/Ex02.java
@@ -3,8 +3,15 @@ package com.asyncLessons;
 public class Ex02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int a = 10;
+		int b = 4;
+		int c = 3;
+		
+		System.out.println("le mie variabili sono a  = " + a + ", b = " + b + ", c = " + c);
+		System.out.println("il risultato di [a + b * c]  è: " + (a + b * c));
+		System.out.println("il risultato di [(a + b) * c]  è: " + ((a + b) * c));
+		System.out.println("il risultato di [a / b + c]  è: " + (a / b + c));
+		System.out.println("il risultato di [a % b]  è: " + (a % b));
 	}
 
 }
