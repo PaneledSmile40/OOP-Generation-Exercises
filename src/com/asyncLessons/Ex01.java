831141f3ea83099efb0ee4e46a18b8492e82023b
PaneledSmile40 <matthew.iis.davinci@gmail.com>
2025-07-16 12:37:35 +0200

    ex01 compleated

--- src/com/asyncLessons/Ex01.java
+++ src/com/asyncLessons/Ex01.java
@@ -3,7 +3,14 @@ package com.asyncLessons;
 public class Ex01 {
 
 	public static void main(String[] args) {
-
+		int a = 5;
+		int b = 10;
+		int temp = 0;
+		
+		a = temp;
+		a = b;
+		b = temp;
+		System.out.println("ora a é: "+a+" mentre b  è: "+b);
 	}
 
 }
