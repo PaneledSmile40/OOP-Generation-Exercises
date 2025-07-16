04ca965e6e0d46cca696101a46d5c54a944de201
PaneledSmile40 <matthew.iis.davinci@gmail.com>
2025-07-16 23:11:45 +0200

    ex04 compleated

--- src/com/asyncLessons/Ex04.java
+++ src/com/asyncLessons/Ex04.java
@@ -1,10 +1,24 @@
 package com.asyncLessons;
 
+import java.util.Scanner;
+
 public class Ex04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		double przNoIva;
+		double ivaPrc;
+		double przSiIva;
+		
+		System.out.println("Inserire prezzo da Ivare");
+		przNoIva = Double.parseDouble(scanner.nextLine());
+		System.out.println("Inserire valore percentuale Iva");
+		ivaPrc = Double.parseDouble(scanner.nextLine());
+		
+		przSiIva = przNoIva + przNoIva * ivaPrc / 100;
+		System.out.println("Il prezzo Ivato è di: " + przSiIva + "€");
+		
+		scanner.close();
 	}
 
 }
