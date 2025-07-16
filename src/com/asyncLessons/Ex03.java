8ca07b88b5b24a86eec4e4237e93498d7a59f44e
PaneledSmile40 <matthew.iis.davinci@gmail.com>
2025-07-16 20:07:10 +0200

    ex03 compleated

--- src/com/asyncLessons/Ex03.java
+++ src/com/asyncLessons/Ex03.java
@@ -3,8 +3,14 @@ package com.asyncLessons;
 public class Ex03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int a = 5;
+		int b = 3;
+		int c = 10;
+
+		boolean result = (a + b * c > 20);
+
+		System.out.println("Result: " + result);
 	}
 
 }
