7fe0f30426293b12e42497e8b3f7e7ecc01752b6
PaneledSmile40 <matthew.iis.davinci@gmail.com>
2025-07-16 12:35:05 +0200

    created all the exercises classes

---
+++ src/com/asyncLessons/Ex01.java
@@ -0,0 +1,9 @@
+package com.asyncLessons;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/com/asyncLessons/Ex01b.java
@@ -0,0 +1,10 @@
+package com.asyncLessons;
+
+public class Ex01b {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
