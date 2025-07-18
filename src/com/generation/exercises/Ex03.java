1c83a68dd67537d15993deb1cbbcbb3f23b3dd07
PaneledSmile40 <matthew.iis.davinci@gmail.com>
2025-07-18 17:52:28 +0200

    ex03 compleated

---
+++ src/com/generation/exercises/Ex03.java
@@ -0,0 +1,18 @@
+package com.generation.exercises;
+
+public class Ex03 {
+
+	public static void main(String[] args) {
+		int c=0;
+		while (c<=30) {
+			System.out.println(c);
+			c+=2;
+		}
+		
+		for(int i=0;i<=30;i+=2) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
