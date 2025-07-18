887b310ad5eaaeae4e3b5cf62e2ca4ccb5940763
PaneledSmile40 <matthew.iis.davinci@gmail.com>
2025-07-18 17:43:42 +0200

    ex01 compleated

---
+++ src/com/generation/exercises/ex01.java
@@ -0,0 +1,18 @@
+package com.generation.exercises;
+
+public class ex01 {
+
+	public static void main(String[] args) {
+		int c=0;
+		while (c<=20) {
+			System.out.println(c);
+			c++;
+		}
+		
+		for(int i=0;i<=20;i++) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
