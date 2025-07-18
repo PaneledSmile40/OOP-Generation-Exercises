614044159bb54c7930cf094138201e76e75bdb6e
PaneledSmile40 <matthew.iis.davinci@gmail.com>
2025-07-18 17:50:59 +0200

    ex02 compleated

--- src/com/generation/exercises/Ex02.java
+++ src/com/generation/exercises/Ex02.java
@@ -1,10 +1,24 @@
 package com.generation.exercises;
 
+import java.util.Scanner;
+
 public class Ex02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int n;
+		int sum=0;
+		
+		System.out.println("Quanti numeri vuoi sommare?");
+		n = sc.nextInt();
+		for(int i=0;i<n;i++) {
+			System.out.println("Inserisci un numero. Numeri attualmente inseriti: " + i);
+			sum += sc.nextInt();
+		}
+		
+		System.out.println("La somma dei numeri inseriti è: " + sum);
+		
+		sc.close();
 	}
 
 }
