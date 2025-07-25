6359b561bd37c1be499d6ddebfde57774f11f46b
PaneledSmile40 <matthew.iis.davinci@gmail.com>
2025-07-25 17:21:29 +0200

    ex03 fix

--- src/com/generation/exercises/ex03/Avvio.java
+++ src/com/generation/exercises/ex03/Avvio.java
@@ -18,24 +18,34 @@ public class Avvio {
 
 		
 
-		for (int c = 0; c <= 3; c++) {
+		for (int c = 0; c < 3; c++) {
+			if(c!=0) {
+				System.out.println("Nome utente errato, tentativi rimanenti " + (3-c));
+			}
 			
 			System.out.println("Inserisci nome utente");
 			username = sc.nextLine();
 			
 			if (user01.checkUser(username)) {
-				for (c = 0; c <= 2; c++) {
+				for (c = 0; c < 2; c++) {
+					if(c!=0) {
+						System.out.println("Nome utente errato, tentativi rimanenti " + (2-c));
+					}
 					System.out.println("Inserisci password");
 					password = sc.nextLine();
 					if (user01.checkPassword(password)) {
 						System.out.println("Accesso Eseguito, benvenuto " + username);
 						return;
 					}
+					
 				}
 			}
 			
 			if (user02.checkUser(username)) {
-				for (c = 0; c <= 2; c++) {
+				for (c = 0; c < 2; c++) {
+					if(c!=0) {
+						System.out.println("Nome utente errato, tentativi rimanenti " + (2-c));
+					}
 					System.out.println("Inserisci password");
 					password = sc.nextLine();
 					if (user02.checkPassword(password)) {
@@ -46,7 +56,10 @@ public class Avvio {
 			}	
 			
 			if (user03.checkUser(username)) {
-				for (c = 0; c <= 2; c++) {
+				for (c = 0; c < 2; c++) {
+					if(c!=0) {
+						System.out.println("Nome utente errato, tentativi rimanenti " + (2-c));
+					}
 					System.out.println("Inserisci password");
 					password = sc.nextLine();
 					if (user03.checkPassword(password)) {
@@ -57,7 +70,10 @@ public class Avvio {
 			}
 			
 			if (user04.checkUser(username)) {
-				for (c = 0; c <= 2; c++) {
+				for (c = 0; c < 2; c++) {
+					if(c!=0) {
+						System.out.println("Nome utente errato, tentativi rimanenti " + (2-c));
+					}
 					System.out.println("Inserisci password");
 					password = sc.nextLine();
 					if (user04.checkPassword(password)) {
@@ -68,7 +84,10 @@ public class Avvio {
 			}
 			
 			if (user05.checkUser(username)) {
-				for (c = 0; c <= 2; c++) {
+				for (c = 0; c < 2; c++) {
+					if(c!=0) {
+						System.out.println("Nome utente errato, tentativi rimanenti " + (2-c));
+					}
 					System.out.println("Inserisci password");
 					password = sc.nextLine();
 					if (user05.checkPassword(password)) {
