0acbd5698802a4cd24a67586ddcfe6bb324da71e
PaneledSmile40 <matthew.iis.davinci@gmail.com>
2025-08-26 14:30:39 +0200

    modulo10_ex01-A2-B_sync

--- src/com/generation/app/Main.java
+++ src/com/generation/app/Main.java
@@ -6,7 +6,34 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println();
+		System.out.println("Quanti numeri vuoi inserire?");
+		int c = sc.nextInt();
+		int[] numeri = new int[c];
+		
+		for(int i=0; i < c;i++) {
+			System.out.println("Inserire numeri interi");
+			numeri[i] = sc.nextInt();;
+		}
+		
+		System.out.println("Stampa array");
+		for(int i=0; i < numeri.length;i++) {
+			System.out.println(numeri[i]);
+		}
+		
+		int somma=0;
+		for(int i=0; i < numeri.length;i++) {
+			somma+=numeri[i];
+		}
+		System.out.println("La somma dei numeri nell'array è: " + somma);
+		
+		int divisore=numeri.length;
+		for(int i=0; i < numeri.length;i++) {
+			if(numeri[i]!=0)continue;
+			divisore--;
+		}
+		double media = somma/divisore;
+		System.out.println("La media dei numeri nell'array escludendo gli zeri è: " + media);
+		
 		sc.close();
 		
 
