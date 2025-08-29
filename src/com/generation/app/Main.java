5356851abb031161ad2c1af31cbd094a64594a70
PaneledSmile40 <matthew.iis.davinci@gmail.com>
2025-08-29 20:40:02 +0200

    ex03 compleated

--- src/com/generation/app/Main.java
+++ src/com/generation/app/Main.java
@@ -1,57 +1,56 @@
 package com.generation.app;
 
-
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Main {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws FileNotFoundException  {
 		
-		Scanner file = new Scanner(new File("res/nomeCognomeEta.txt"));
-		String stringedFile = file.nextLine();
-		String[] datiPersone = stringedFile.split(",");			//i dati di ogni persona sono in 1 unico campo
-		String[][] persone = new String[datiPersone.length][3]; //ogni riga della matrice rappresenta 1 persona; la colonna 0 è il nome, la colonna 1 è il cognome, la colonna 2 è l'età
+		Scanner file = new Scanner(new File("res/modelliMacchine.txt"));
 		
-		for(int i=0; i < datiPersone.length; i++ ) {
-			persone[i] = datiPersone[i].split(":");
+		Scanner tastiera = new Scanner(System.in);
+		
+		List<String>cars = new ArrayList<>();
+		
+		while(file.hasNextLine()) {
+			cars.add(file.nextLine());
 		}
 		
-//		for(int i = 0; i<3;i++) {
-//			System.out.println(persone[i][0]);
-//		}
-		System.out.println("Nomi:");
-		for(String[] riga:persone)
-			System.out.println(riga[0]);
+		String input;
 		
-		System.out.println("\nCognomi:");
-		for(String[] riga:persone)
-			System.out.println(riga[1]);
-		
-		System.out.println("\nEta maggiore di 60:");
-		for(String[] riga:persone) {
-			if(Integer.parseInt(riga[2]) > 60){
-				for(String c:riga) {
-					System.out.print(c + " ");
+		do{
+			System.out.println("Cerca un modello di auto");
+			input = tastiera.nextLine();
+			
+			
+			boolean exist = false;
+			for (String str: cars) {
+				if(str.equalsIgnoreCase(input)) {
+					exist = true;
+					break;
 				}
 			}
-			System.out.println();
-		}
+			
+			String message = exist?
+					input+" è presente nella lista":
+					input+" non è presente nella lista";
+			
+			System.out.println(message);
+			
+			System.out.println("\nVuoi cercare un altro modello? (S/N)");
+			input = tastiera.nextLine();
+			
+		}while(input.equalsIgnoreCase("S"));
 		
-		System.out.println("\nLa media delle età comprese tra 30 e 50 è: ");
-		int c = 0;
-		int somma = 0;
-		for(String[] riga:persone) {
-			if(Integer.parseInt(riga[2]) >= 30 && Integer.parseInt(riga[2]) <= 50) {
-				somma += Integer.parseInt(riga[2]);
-				c++;
-			}
-		}
-		System.out.println( (double)(somma) / c );
-		
+		System.out.println("\nGrazie e arrivederci");
 		
 		file.close();
+		tastiera.close();
+
 	}
 
 }
