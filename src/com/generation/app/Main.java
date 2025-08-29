f16cf5d07de5d0617c3f75266921a4b17723bc93
PaneledSmile40 <matthew.iis.davinci@gmail.com>
2025-08-29 12:47:47 +0200

    compleated

--- src/com/generation/app/Main.java
+++ src/com/generation/app/Main.java
@@ -1,15 +1,29 @@
 package com.generation.app;
 
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println();
-		sc.close();
+	public static void main(String[] args) throws FileNotFoundException {
 		
-
+		Scanner file = new Scanner(new File("res/numeri.txt")); 
+		String[] stringedDataArray = file.nextLine().split(",");
+		file.close();
+		
+		int[] dataArray = new int[stringedDataArray.length];
+		
+		for(int i=0; i<dataArray.length;i++) {
+			dataArray[i] = Integer.parseInt(stringedDataArray[i]);
+		}
+		
+		for(int num:dataArray) {
+			System.out.println(num);
+		}
+		
+		
+		
 	}
 
 }
