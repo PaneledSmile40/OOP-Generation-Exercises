bcab2aecd348c8a870d19f0864f5944d671997cd
PaneledSmile40 <matthew.iis.davinci@gmail.com>
2025-08-29 16:28:53 +0200

    ex02 compleated

--- src/com/generation/app/Main.java
+++ src/com/generation/app/Main.java
@@ -1,5 +1,6 @@
 package com.generation.app;
 
+
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -8,31 +9,49 @@ public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		
-		String riga = new String();
-		Scanner file = new Scanner(new File("res/numeri.txt")); 
-		while(file.hasNextLine()) {
-			String temp = file.nextLine();
-			if(temp.charAt(0) == ',') {
-				temp = temp.substring(1);
-			}
-			if(temp.charAt(temp.length() - 1)!=',') {
-				temp += ",";
-			}
-			riga += temp;	
-		}
-		riga = riga.substring(0, riga.length() - 1);
-		file.close();
+		Scanner file = new Scanner(new File("res/nomeCognomeEta.txt"));
+		String stringedFile = file.nextLine();
+		String[] datiPersone = stringedFile.split(",");			//i dati di ogni persona sono in 1 unico campo
+		String[][] persone = new String[datiPersone.length][3]; //ogni riga della matrice rappresenta 1 persona; la colonna 0 è il nome, la colonna 1 è il cognome, la colonna 2 è l'età
 		
-		String[] stringedDataArray = riga.split(",");
-		int[] dataArray = new int[stringedDataArray.length];
-		for (int i = 0; i < dataArray.length; i++) {
-			dataArray[i] = Integer.parseInt(stringedDataArray[i]);
+		for(int i=0; i < datiPersone.length; i++ ) {
+			persone[i] = datiPersone[i].split(":");
 		}
-
-		for (int num : dataArray) {
-			System.out.println(num);
-		}	
-
+		
+//		for(int i = 0; i<3;i++) {
+//			System.out.println(persone[i][0]);
+//		}
+		System.out.println("Nomi:");
+		for(String[] riga:persone)
+			System.out.println(riga[0]);
+		
+		System.out.println("\nCognomi:");
+		for(String[] riga:persone)
+			System.out.println(riga[1]);
+		
+		System.out.println("\nEta maggiore di 60:");
+		for(String[] riga:persone) {
+			if(Integer.parseInt(riga[2]) > 60){
+				for(String c:riga) {
+					System.out.print(c + " ");
+				}
+			}
+			System.out.println();
+		}
+		
+		System.out.println("\nLa media delle età comprese tra 30 e 50 è: ");
+		int c = 0;
+		int somma = 0;
+		for(String[] riga:persone) {
+			if(Integer.parseInt(riga[2]) >= 30 && Integer.parseInt(riga[2]) <= 50) {
+				somma += Integer.parseInt(riga[2]);
+				c++;
+			}
+		}
+		System.out.println( (double)(somma) / c );
+		
+		
+		file.close();
 	}
 
 }
